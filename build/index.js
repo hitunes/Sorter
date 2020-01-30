110a9652167c3a73eac5253c893250b4e553dad4
hitunes <hitunes247@gmail.com>
2020-01-30 13:26:23 +0200

    Sort array of numbers and string

--- build/index.js
+++ build/index.js
@@ -1,5 +1,10 @@
 "use strict";
-console.log('hi there');
-var logSomething = function () {
-    console.log('something');
-};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([-1,3,50,23,7,9,8])
+// const sorter = new Sorter(numbersCollection)
+var charactersCollection = new CharactersCollection_1.CharactersCollection('Xaaayb');
+var sorter = new Sorter_1.Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-console.log('hi there')
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const logSomething = () => {
-  console.log('something')
-}
+// const numbersCollection = new NumbersCollection([-1,3,50,23,7,9,8])
+// const sorter = new Sorter(numbersCollection)
+
+const charactersCollection = new CharactersCollection('Xaaayb')
+const sorter = new Sorter(charactersCollection)
+sorter.sort();
+console.log(charactersCollection.data)
